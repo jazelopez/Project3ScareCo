312767b9a83c6c67be3d3d8d21dd23e76acccf66
jazelopez <jazbelopez@gmail.com>
2021-11-27 20:40:06 -0600

    Almost got the menu to work

---
+++ Assets/Scripts/SwitchScence1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SwitchScence1 : MonoBehaviour
+{
+    //load big scene
+    void onTriggerEnter(Collider other){
+        SceneManager.LoadScene(0); 
+    }
+}
---
+++ Assets/Scripts/SwitchScene2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene2 : MonoBehaviour
+{
+    void onTriggerEnter(Collider other){
+        SceneManager.LoadScene(1); 
+    }
+}
